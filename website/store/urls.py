c4854853a117f631d4cccdec14ee9e0d1585fc8a
Selim <selimaydi@gmail.com>
2017-10-05 16:25:49 +0200

    Update urls.py

--- website/store/urls.py
+++ website/store/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^contact/$', views.contact, name='contact'), #url = ip/contact/ of ip/contact (de eerste / is niet nodig)
     url(r'^faq$', views.faq, name='faq'),
     url(r'^product$', views.product, name='product'),
-    url(r'^.well-known/$', views.index, name='.well-known'),
+    url(r'^about$', views.about, name='about'),
     url(r'^product/(?P<item>\d+)$', views.product2, name='product2'), #url = /product/{item}, \d+ betekent 1 nummer of meer
     url(r'^testing$', views.testing, name='testing'),
     url(r'^logout/$', views.logoutview, name='logout'),
