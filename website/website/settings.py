f2533624a518a74b53134d7a4db511f2f6d10967
KyrumX <kyrumx@hotmail.nl>
2017-10-06 12:32:58 +0200

    Some changes

--- website/website/settings.py
+++ website/website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'j^+tj99w@m46g^n2$q$+vdah2y&-)_--_mxkcfqbgpl*sq@58)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
