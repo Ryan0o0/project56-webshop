fe052981aa56ba8ab2448d739a8a1b41ab36e7de
vitalynx <selimaydi@gmail.com>
2017-10-21 18:50:32 +0200

    shoppingcart styling

--- website/store/templatetags/shoppingcart_tags.py
+++ website/store/templatetags/shoppingcart_tags.py
@@ -9,16 +9,17 @@ register = template.Library()
 def displayCart(sessionkey):
     length = cartLength(sessionkey)
 
-    html = ""
+    html = "<ul class='cartwrap'>"
 
     for e in ShoppingCart.objects.all().filter(session_key=sessionkey):
         #IMG - NAAM - AANTAL - PRIJS
-        html += "<img src='" + getProdImage(e.prodNum) + "' id='zoom_05' data-zoom-image='https://i.pinimg.com/736x/86/ff/e2/86ffe2b49daf0feed78a1c336753696d--black-panther-comic-digital-comics.jpg' style='width:150px;height:160px;'>"
-        html += " " + e.prodNum.prodName
-        html += " Aantal: " + str(e.amount)
-        html += " Prijs: " + str(e.amount * e.prodNum.prodPrice)
+        html += "<li class='cartitem'><div class='productcartimg'><img src='" + getProdImage(e.prodNum) + "' id='zoom_05' data-zoom-image='https://i.pinimg.com/736x/86/ff/e2/86ffe2b49daf0feed78a1c336753696d--black-panther-comic-digital-comics.jpg'></div>"
+        html += "<div class='textplace'><button class='remove'><i class='fa fa-trash' aria-hidden='true'></i><p>verwijderen</p></button><button class='movetowishlist'><i class='fa fa-heart' aria-hidden='true'></i><p>verplaats naar verlanglijstje</p></button><p class='title'>" + e.prodNum.prodName + "</p>"
+        html += "<p>Aantal: " + str(e.amount) + "</p>"
+        html += "<p>Prijs: €" + str(e.amount * e.prodNum.prodPrice) + "</p>"
+        html += "</div></li>"
 
-        html += "<br/>"
+    html += "</ul>"
     return html
 
 
