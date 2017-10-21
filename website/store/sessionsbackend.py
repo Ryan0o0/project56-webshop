56879a1c195f98b5b0af87f2bb361eb9f748be43
KyrumX <kyrumx@hotmail.nl>
2017-10-21 08:50:58 +0200

    A comment

--- website/store/sessionsbackend.py
+++ website/store/sessionsbackend.py
@@ -1,5 +1,7 @@
 from django.contrib.sessions.backends.db import SessionStore as DbSessionStore
 
 class SessionStore(DbSessionStore):
+    #TODO: Zorg dat er wel een nieuwe session komt naar dat de data (winkelwagentje e.d.) wordt overgezet. Nu wordt heel de session niet vervangen.
+
     def cycle_key(self):
         pass
