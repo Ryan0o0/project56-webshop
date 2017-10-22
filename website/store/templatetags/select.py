e77f55998797ba383efd3d886cf5d91e447d9216
vitalynx <selimaydi@gmail.com>
2017-10-22 17:43:29 +0200

    product amount selector

---
+++ website/store/templatetags/select.py
@@ -0,0 +1,13 @@
+amount = 4
+amounttxt = "<select name='amount'>"
+for i in range(12):
+    if i == amount:
+        amounttxt += "<option selected>" + str(amount) + "</option>"
+    else:
+        if i == 11:
+            amounttxt += "<option>meer..</option>"
+        else:
+            amounttxt += "<option>" + str(i) + "</option>"
+amounttxt += "</select>"
+
+print(amounttxt)
--- website/store/templatetags/shoppingcart_tags.py
+++ website/store/templatetags/shoppingcart_tags.py
@@ -8,18 +8,33 @@ register = template.Library()
 @register.simple_tag()
 def displayCart(sessionkey):
     length = cartLength(sessionkey)
+    print(length)
+    if length == 0:
+        html = "<p class='title'>Momenteel zit er nog niks in je winkelwagentje.</p>"
+    else:
+        html = "<ul class='cartwrap'>"
+        cnt = 0
+        for e in ShoppingCart.objects.all().filter(session_key=sessionkey):
+            amounttxt = "<select name='amount' id='amountselect" + str(cnt) + "' class='amountselect'>"
+            for i in range(1, 12):
+                if i == e.amount:
+                    amounttxt += "<option selected>" + str(e.amount) + "</option>"
+                else:
+                    if i == 11:
+                        amounttxt += "<option>meer..</option>"
+                    else:
+                        amounttxt += "<option>" + str(i) + "</option>"
+            amounttxt += "</select><input type='text' id='meer" + str(cnt) + "' value='" + str(e.amount) + "' style='display: none;' class='amountinput' />"
 
-    html = "<ul class='cartwrap'>"
+            #IMG - NAAM - AANTAL - PRIJS
+            html += "<li class='cartitem'><div class='productcartimg'><img src='" + getProdImage(e.prodNum) + "' id='zoom_05' data-zoom-image='https://i.pinimg.com/736x/86/ff/e2/86ffe2b49daf0feed78a1c336753696d--black-panther-comic-digital-comics.jpg'></div>"
+            html += "<div class='textplace'><button name='removeFromCartButton' value='" + str(e.prodNum) +"' class='remove'><i class='fa fa-trash' aria-hidden='true'></i><p>verwijderen</p></button><button class='movetowishlist'><i class='fa fa-heart' aria-hidden='true'></i><p>verplaats naar verlanglijstje</p></button><p class='title'>" + e.prodNum.prodName + "</p>"
+            html += "<p>Aantal: " + amounttxt + "</p>"
+            html += "<p>Prijs: €" + str(e.amount * e.prodNum.prodPrice) + "</p>"
+            html += "</div></li>"
+            cnt += 1
+        html += "</ul>"
 
-    for e in ShoppingCart.objects.all().filter(session_key=sessionkey):
-        #IMG - NAAM - AANTAL - PRIJS
-        html += "<li class='cartitem'><div class='productcartimg'><img src='" + getProdImage(e.prodNum) + "' id='zoom_05' data-zoom-image='https://i.pinimg.com/736x/86/ff/e2/86ffe2b49daf0feed78a1c336753696d--black-panther-comic-digital-comics.jpg'></div>"
-        html += "<div class='textplace'><button name='removeFromCartButton' value='" + str(e.prodNum) +"' class='remove'><i class='fa fa-trash' aria-hidden='true'></i><p>verwijderen</p></button><button class='movetowishlist'><i class='fa fa-heart' aria-hidden='true'></i><p>verplaats naar verlanglijstje</p></button><p class='title'>" + e.prodNum.prodName + "</p>"
-        html += "<p>Aantal: " + str(e.amount) + "</p>"
-        html += "<p>Prijs: €" + str(e.amount * e.prodNum.prodPrice) + "</p>"
-        html += "</div></li>"
-
-    html += "</ul>"
     return html
 
 
