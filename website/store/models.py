d5d72fe0667290c73292fb38adbc1d8ac599d73b
0896320 <0896320@hr.nl>
2017-10-24 12:33:18 +0200

    broken models

--- website/store/models.py
+++ website/store/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import  post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -7,12 +9,19 @@ class Customers(models.Model):
     class Meta:
         verbose_name_plural = "Customers"
 
-    customerID = models.IntegerField(primary_key=True)
+    customerID = models.OneToOneField(User, primary_key=True, db_column='customerID')
     email = models.CharField(max_length=100)
     name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     telephone = models.CharField(max_length=12)
     isRegistered = models.BooleanField()
+
+
+    def create_profile(sender, **kwargs):
+        if kwargs['created']:
+            user_profile = Customers.objects.create(customerID=kwargs['instance'], isRegistered=True)
+
+    post_save.connect(create_profile, sender=User)
 
 class Address(models.Model):
     class Meta:
