9d9df1cfabf22c2d9126b2ece221f2c019f8e00e
KyrumX <kyrumx@hotmail.nl>
2017-11-16 15:00:08 +0100

    Fixed ENTER

--- website/store/collections/posts.py
+++ website/store/collections/posts.py
@@ -3,4 +3,7 @@ from django.shortcuts import redirect, render
 from ..database.getData import queryVerbeterFunctie
 
 def searchPost(request):
+    query = request.POST.get('searchtext')
+    if len(query) == 0:
+        return redirect('/')
     return redirect("/search/" + queryVerbeterFunctie(str(request.POST.get('searchtext'))))
