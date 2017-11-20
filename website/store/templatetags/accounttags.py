0e91f9bd8e329893d1de733381d9e96014367ade
Vitalynx <selimaydi@gmail.com>
2017-11-20 22:37:22 +0100

    orderdetails update

--- website/store/templatetags/accounttags.py
+++ website/store/templatetags/accounttags.py
@@ -1,6 +1,8 @@
 from django import template
 
 from store.models import OrderDetails
+from store.models import Products
+from store.models import ProductDetails
 from ..database.AccountOps import *
 
 register = template.Library()
@@ -11,10 +13,10 @@ def displayorders(request):
     if getOrderAmount(request) == 0:
         html = "Je hebt nog geen orders geplaatst bij ons."
     else:
-        html = "<table><tbody><tr><th>Order Nummer</th><th>Datum Geplaatst</th><th>Status</th></tr>"
+        html = "<div class='table1'><table><tbody><tr><th>Order Nummer</th><th>Datum Geplaatst</th><th>Status</th></tr>"
         for e in Orders.objects.all().filter(customerID=Customers(request.user.id)):
-            html = html + "<tr><td>" + str(e.orderNum) + "</td><td>" + str(e.orderDate) + "</td><td>" + e.orderStatus + "</td></tr>"
-        html = html + "</tbody></table>"
+            html = html + "<tr><td><a href='bestelling?ordernum=" + str(e.orderNum) +"'><i class='fa fa-external-link' aria-hidden='true'></i></a>" + str(e.orderNum) + "</td><td>" + str(e.orderDate) + "</td><td>" + e.orderStatus + "</td></tr>"
+        html = html + "</tbody></table></div>"
 
     return html
 
@@ -22,20 +24,22 @@ def displayorders(request):
 def displayuser(request):
 	object = Customers.objects.get(customerID = request.user.id)
 	object2 = Address.objects.get(customerID = request.user.id)
-	html = "<table><tbody>"
+	html = "<div class='table1'><table><tbody>"
 	html += "<tr><th>Voornaam</th><td>{0}</td></tr><tr><th>Achternaam</th><td>{1}</td></tr><tr><th>Email</th><td>{2}</td></tr><tr><th style='border-bottom-left-radius: 5px;'>Adres</th><td>{3} {4} {5} {6}</td></tr>".format(object.name, object.surname, object.email, object2.address, str(object2.number), object2.city, str(object2.postalcode))
-	html += "</tbody></table>"
+	html += "</tbody></table></div>"
 	return html
 
 @register.simple_tag()
 def displayorderdetails(ordernum):
     ordernum = int(ordernum)
     object = Orders.objects.get(orderNum=ordernum)
-    html = "<b> Status: " + object.orderStatus + "</b> | Datum: " + str(object.orderDate)
+    html = "<div class='table1'><table><tbody><tr><th>Ordernummer</th><td>" + str(ordernum) +"</td></tr><tr><th>Status</th><td>" + object.orderStatus + "</td></tr><tr><th style='border-bottom-left-radius: 5px; border: none;'>Datum</th><td>" + str(object.orderDate) + "</td></tr></tbody></table></div>"
+    html += "</div><div class='aboutsection'><div class='table1'><table><tbody><tr><th style='border-top-left-radius: 5px; border: none;'>Productnummer</th><th>Naam</th><th>Prijs</th><th style='border-top-right-radius: 5px; border: none;'>Aantal</th></tr>"
     for e in OrderDetails.objects.all().filter(orderNum=ordernum):
-        html = html + addProductDetail(e)
-    return html
-
-def addProductDetail(e):
-    html = "</br> productnummer: " + str(e.productNum) + " Aantal: " + str(e.amount)
+        product = Products.objects.get(prodNum = e.getNum())
+        productdetail = ProductDetails.objects.get(prodNum = e.getNum())
+        print(type(e.getNum()))
+        print("productnum:" + str(e.getNum()))
+        html = html + "<tr><td>" + str(e.productNum) + "</td><td><a href='/product/" + str(e.productNum) + "'>" + "<img src='" + str(productdetail.imageLink) +"'></a><p>" + str(product.prodName) + "</p></td><td>€ " + str(product.prodPrice) + "</td><td>" + str(e.amount) + "</td></tr>"
+    html += "</tr></tbody></table></div>"
     return html
