47ef7f31d448a3b92b5673182c8cfd0cf62f2269
KyrumX <kyrumx@hotmail.nl>
2017-11-27 15:06:22 +0100

    Removed some prints

--- website/store/requests/posts.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import redirect, render
-
-from website.store.database.getData import queryVerbeterFunctie
-
-
-def searchPost(request):
-    return redirect("search/" + queryVerbeterFunctie(str(request.POST.get('searchtext'))))
