3337bc40376692920b5cf049fafac56a75d26fc7
Selim <selimaydi@gmail.com>
2017-12-05 16:26:34 +0100

    Update formvalidators.py

--- website/store/validators/formvalidators.py
+++ website/store/validators/formvalidators.py
@@ -8,21 +8,21 @@ def postalcode_validator(postalcode):
     else:
         for i in range(0, 4):
             if not (str(postalcode[i]).isdigit()):
-                raise ValidationError(_('eerste 4 characters moeten cijfers zijn.'))
+                raise ValidationError(_('Eerste 4 characters moeten cijfers zijn.'))
         for i in range(4, 5):
             if not (str(postalcode[i]).isalpha()):
-                raise ValidationError(_('laatste 2 characters moeten hoofdletters zijn'))
+                raise ValidationError(_('Laatste 2 characters moeten hoofdletters zijn'))
 
 def telephone_validator(telephone):
     if len(telephone) != 8 and len(telephone) != 10:
-        raise ValidationError(_('telefoonnummer is niet geldig!'))
+        raise ValidationError(_('Telefoonnummer is niet geldig!'))
     else:
         if not(str(telephone)).isdigit():
-            raise ValidationError(_('telefoonnummer is niet geldig'))
+            raise ValidationError(_('Telefoonnummer is niet geldig'))
 
 def creditcard_validator(cardnumber, length):
     if (len(str(cardnumber))) != length:
-        raise ValidationError(_('kaarnummer is niet geldig!'))
+        raise ValidationError(_('Kaartnummer is niet geldig!'))
     # else:
     #     for i in range(len(cardnumber)):
     #         if not str(cardnumber[i]).isdigit():
