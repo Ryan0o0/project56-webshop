072cfa677519ec7c93811674309eb1300e066d7a
Aaron <kyrumx@hotmail.nl>
2017-12-16 11:22:59 +0100

    Fixed 0 in Month

--- website/store/urls.py
+++ website/store/urls.py
@@ -40,5 +40,5 @@ urlpatterns = [
     url(r'^admin/edit/user/(?P<userid>\d+)/$', adminviews.EditUser.as_view(), name="edituser"),
     url(r'^admin/createproduct/$', adminviews.createproduct, name="createproduct"),
     url(r'^admin/data/products/$', adminviews.ProductGraphSelection.as_view(), name="productdata"),
-    url(r'^admin/data/products/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', adminviews.ProductGraphMonth.as_view(), name="productdatamonth"),
+    url(r'^admin/data/products/(?P<year>[0-9]{4})/(?P<month>[1-9]{1,2})/$', adminviews.ProductGraphMonth.as_view(), name="productdatamonth"),
 ]
